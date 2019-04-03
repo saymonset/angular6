9ab61a3cd37ce9232433f1b85f9564189aee0c90
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2019-04-03 14:20:50 -0400

    Observable

--- src/app/messages/message.component.ts
+++ src/app/messages/message.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { MessageService } from "./message.service";
 import { Message } from "./message.model";
+import { Observable } from "rxjs";
 
 @Component({
 	selector: "paMessages",
@@ -10,6 +11,6 @@ export class MessageComponent {
 	lastMessage: Message;
 	
 	constructor(messageService: MessageService) {
-		messageService.registerMessageHandler(m => this.lastMessage = m);
+		messageService.messages.subscribe(m => this.lastMessage = m);
 	}
 }
