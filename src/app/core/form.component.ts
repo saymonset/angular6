5ebdbd4532fd9be4b2a488b243732f45ece19201
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2019-04-03 11:52:58 -0400

    Observable edit create product

--- src/app/core/form.component.ts
+++ src/app/core/form.component.ts
@@ -1,8 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, Inject } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Product } from "../model/product.model";
 import { Model } from "../model/repository.model"
-import { MODES, SharedState } from "./sharedState.model";
+import { MODES, SharedState, SHARED_STATE } from "./sharedState.model";
+import { Observable } from "rxjs";
 
 @Component({
 	selector: "paForm",
@@ -15,13 +16,18 @@ export class FormComponent {
 
 	product: Product = new Product();
 	
-	constructor(private model: Model,private state: SharedState) { 
+	constructor(private model: Model,@Inject(SHARED_STATE) private stateEvents: Observable<SharedState>) {
 
+	stateEvents.subscribe((update) => {
+		this.product = new Product();
+			if (update.id != undefined) {
+				Object.assign(this.product, this.model.getProduct(update.id));
+			}
+		this.editing = update.mode == MODES.EDIT;
+		});
 	}
+	editing: boolean = false;
 		
-		get editing(): boolean {
-			return this.state.mode == MODES.EDIT;
-		}
 	
 	submitForm(form: NgForm) {
 		if (form.valid) {
